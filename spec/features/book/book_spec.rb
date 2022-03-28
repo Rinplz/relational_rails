50d4dd18e45fa017ea03adf826ea1c2054b4a3af
Rin Fabian <pfabianschool@gmail.com>
2022-03-28 11:22:16 -0500

    test; #book_recipe_index

--- spec/features/book/book_spec.rb
+++ spec/features/book/book_spec.rb
@@ -21,4 +21,17 @@ RSpec.describe "book pages", type: :feature do
     expect(page).to have_content("Rating: #{book_1.rating}/5")
     expect(page).to have_content("Series: #{book_1.author}")
   end
+
+  it 'can show all recipes in a book' do
+    book_1 = Book.create!(title: 'week night dinners', book_completed: false, rating: 4, author: 'taste of home')
+    recipe_1 = Recipe.create!(name: 'cajun salmon',ingredient_count: 3, ingredient_list: 'salmon, cajun seasoning, butter', book_id: Book.first.id)
+    recipe_2 = Recipe.create!(name: 'nikujaga',ingredient_count: 4, ingredient_list: 'beef, potatoes, carrots, green beans', book_id: Book.first.id)
+
+    visit "/books/#{book_1.id}/recipes"
+
+    expect(page).to have_content(recipe_1.name)
+    expect(page).to have_content(recipe_2.name)
+    expect(page).to have_content("ingredients needed: #{recipe_1.ingredient_count}| #{recipe_1.ingredient_list}")
+    expect(page).to have_content("ingredients needed: #{recipe_2.ingredient_count}| #{recipe_2.ingredient_list}")
+  end
 end
