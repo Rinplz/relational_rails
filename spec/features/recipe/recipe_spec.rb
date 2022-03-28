16cfaa4cbe8677b2174e7496cdca453005ae3915
Rin Fabian <pfabianschool@gmail.com>
2022-03-28 10:41:31 -0500

    changed syntax of recipe variables in tests to appropriately pull foreign key

--- spec/features/recipe/recipe_spec.rb
+++ spec/features/recipe/recipe_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe "recipe pages", type: :feature do
   it 'can see all recipes and their attributes' do
-    book_1 = Book.create(title: 'week night dinners', book_completed: false, rating: 4, author: 'taste of home')
-    recipe_1 = book_1.Recipe.create(name: 'cajun salmon',ingredient_count: 3, ingredient_list: 'salmon, cajun seasoning, butter')
-    recipe_2 = book_1.Recipe.create(name: 'nikujaga',ingredient_count: 4, ingredient_list: 'beef, potatoes, carrots, green beans')
+    book_1 = Book.create!(title: 'week night dinners', book_completed: false, rating: 4, author: 'taste of home')
+    recipe_1 = Recipe.create!(name: 'cajun salmon',ingredient_count: 3, ingredient_list: 'salmon, cajun seasoning, butter', book_id: Book.first.id)
+    recipe_2 = Recipe.create!(name: 'nikujaga',ingredient_count: 4, ingredient_list: 'beef, potatoes, carrots, green beans', book_id: Book.first.id)
 
     visit '/recipes'
 
