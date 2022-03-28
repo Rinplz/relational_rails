6228f2e1be4d515f26724f63c5d5a7cb7b2836f5
Rin Fabian <pfabianschool@gmail.com>
2022-03-27 22:58:10 -0500

    created spec for books

---
+++ spec/models/book_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe Book, type: :model do
+  describe 'instance methods' do
+    
+  end
+end
---
+++ spec/models/recipe_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe Recipe, type: :model do
+  describe 'instance methods' do
+
+  end
+end
