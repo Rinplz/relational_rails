a211cc3c98dfd68b2b7ec60a572c58c5b4594e73
Rin Fabian <pfabianschool@gmail.com>
2022-03-28 03:00:21 -0500

    created #show test for books

--- spec/features/book/index_spec.rb
+++ spec/features/book/index_spec.rb
@@ -10,4 +10,15 @@ RSpec.describe "book index page", type: :feature do
     expect(page).to have_content(book_1.title)
     expect(page).to have_content(book_2.title)
   end
+
+  it 'can show attributes when searched by id' do
+    book_1 = Book.create(title: 'air fryer recipes', book_completed: false, rating: 5, author: 'allrecipe')
+    book_2 = Book.create(title: 'week night dinners', book_completed: false, rating: 4, author: 'taste of home')
+
+    visit "/books/#{book_1.id}"
+
+    expect(page).to have_content(book_1.title)
+    expect(page).to have_content("Rating: #{book_1.rating}/5")
+    expect(page).to have_content("Series: #{book_1.author}")
+  end
 end
