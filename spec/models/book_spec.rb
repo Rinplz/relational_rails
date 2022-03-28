e507c433a290fe8f3c4b3426dacdab5d97ae71b4
Rin Fabian <pfabianschool@gmail.com>
2022-03-27 23:29:07 -0500

    created tests for validating the attributes of each table

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,7 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  describe 'instance methods' do
-    
+  describe 'validates attributes' do
+    validates_presence_of :id
+    validates_presence_of :title
+    validates_presence_of :created_at
+    validates_presence_of :updated_at
+    validates_presence_of :book_completed?
+    validates_presence_of :rating
+    validates_presence_of :author
   end
 end
--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,7 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Recipe, type: :model do
-  describe 'instance methods' do
+  describe 'validates attributes' do
+    validates_presence_of :id
+    validates_presence_of :name
+    validates_presence_of :created_at
+    validates_presence_of :updated_at
+    validates_presence_of :has_been_made_once?
+    validates_presence_of :ingredient_count
+    validates_presence_of :ingredients
 
   end
 end
