e0f0caa35f447e62124dd67a57c2423612c98b52
Rin Fabian <pfabianschool@gmail.com>
2022-03-28 12:35:30 -0500

    created test for link to recipe#index on each page

--- spec/features/recipe/recipe_spec.rb
+++ spec/features/recipe/recipe_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe "recipe pages", type: :feature do
     @book_1 = Book.create!(title: 'week night dinners', book_completed: false, rating: 4, author: 'taste of home')
     @recipe_1 = Recipe.create!(name: 'cajun salmon',ingredient_count: 3, ingredient_list: 'salmon, cajun seasoning, butter', book_id: Book.first.id)
     @recipe_2 = Recipe.create!(name: 'nikujaga',ingredient_count: 4, ingredient_list: 'beef, potatoes, carrots, green beans', book_id: Book.first.id)
-
+    @views = ['/recipes', "/recipes/#{@recipe_1.id}"]
 
   end
   it 'can see all recipes and their attributes' do
@@ -20,10 +20,22 @@ RSpec.describe "recipe pages", type: :feature do
   end
 
   it 'can show a specific recipe and its attributes' do
-    
+
       visit "/recipes/#{@recipe_1.id}"
 
       expect(page).to have_content(@recipe_1.name)
       expect(page).to have_content("ingredients needed: #{@recipe_1.ingredient_count}| #{@recipe_1.ingredient_list}")
+  end
+
+  it 'has a link to index on each page' do
+    @views.each do |view|
+      visit view
+
+      expect(page).to have_link('Recipes Index')
+      click_link('Recipes Index')
+
+      expect(page).to have_content(@recipe_1.name)
+      expect(page).to have_content(@recipe_2.name)
+    end
   end
 end
