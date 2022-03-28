940ea4ea6725c20f42b32457f6db21a03c72c18d
Rin Fabian <pfabianschool@gmail.com>
2022-03-28 11:10:38 -0500

    test; #show for recipes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/books', to: 'books#index'
   get '/books/:id', to: 'books#show'
+  get '/recipes', to: 'recipes#index'
+  get '/recipes/:id', to: 'recipes#show'
 end
--- spec/features/recipe/recipe_spec.rb
+++ spec/features/recipe/recipe_spec.rb
@@ -10,8 +10,19 @@ RSpec.describe "recipe pages", type: :feature do
 
     expect(page).to have_content(recipe_1.name)
     expect(page).to have_content(recipe_2.name)
-    expect(page).to have_content("ingredients needed: #{recipe_1.ingredient_list}")
-    expect(page).to have_content("ingredients needed: #{recipe_2.ingredient_list}")
+    expect(page).to have_content("ingredients needed: #{recipe_1.ingredient_count}| #{recipe_1.ingredient_list}")
+    expect(page).to have_content("ingredients needed: #{recipe_2.ingredient_count}| #{recipe_2.ingredient_list}")
 
+  end
+
+  it 'can show a specific recipe and its attributes' do
+      book_1 = Book.create!(title: 'week night dinners', book_completed: false, rating: 4, author: 'taste of home')
+      recipe_1 = Recipe.create!(name: 'cajun salmon',ingredient_count: 3, ingredient_list: 'salmon, cajun seasoning, butter', book_id: Book.first.id)
+      recipe_2 = Recipe.create!(name: 'nikujaga',ingredient_count: 4, ingredient_list: 'beef, potatoes, carrots, green beans', book_id: Book.first.id)
+
+      visit "/recipes/#{recipe_1.id}"
+
+      expect(page).to have_content(recipe_1.name)
+      expect(page).to have_content("ingredients needed: #{recipe_1.ingredient_count}| #{recipe_1.ingredient_list}")
   end
 end
